873f9471801722e4cdd543036343b5e1b9cb11d2
Invisible-gudegude <59083810+invesible-gude@users.noreply.github.com>
2020-02-26 14:18:05 +0700

    Initial commit

---
+++ globals/Config.js
@@ -0,0 +1,10 @@
+const internal = {};
+
+module.exports = internal.Global =  class {
+    constructor() { 
+        this.SERVER_URL = 'http://localhost:5001' 
+        this.CLIENT_URL = 'http://localhost:3000' 
+    } 
+ 
+   
+}
---
+++ models/BaseModel.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    connectionimit: 100,
+    host: 'localhost',
+    user: 'root',
+    port: 3306, 
+    password: '',
+    database: 'dol_db',
+    multipleStatements: true
+}); 
+/* 
+var connection = mysql.createConnection({
+    connectionimit: 100,
+   host: 'database-rvscs.cdaloohdepaj.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    port: 3306,
+    password: 'root123456',
+    database: 'revel-market_db',
+    multipleStatements: true
+});
+ */
+
+connection.connect();
+
+module.exports = connection;
